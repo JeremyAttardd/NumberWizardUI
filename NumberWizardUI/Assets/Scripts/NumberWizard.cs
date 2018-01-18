b272e359d287315507060585dd2eaef85a04a56f
JeremyAttardd <32672976+JeremyAttardd@users.noreply.github.com>
2018-01-18 19:17:34 +0100

    NumberWizard

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class NumberWizard : MonoBehaviour {
+
+
+    // Declare 2 integer variables
+    int min = 1;
+    int max = 1001;
+    int guess = 500;
+    int maxNumberOfTries = 10;
+
+    public Text guessText;
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min, max);
+
+        guessText.text = guess.ToString();
+
+        maxNumberOfTries--;
+        if (maxNumberOfTries == 0) // If AI does not guess the number in 10 tries
+        {
+            SceneManager.LoadScene("Win"); // Load Win Scene
+        }
+    }
+                            
+    // Use this for initialization
+    void Start() {
+
+        }
+	
+	
+}
